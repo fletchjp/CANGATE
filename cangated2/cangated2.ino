305c1101ccc8013deb3fe2c504f53855fee39d93
John Fletcher <john@bunbury28.plus.com>
2023-10-07 12:06:27 +0100

    Update cangated2.ino
    Change button numbers

--- cangated2/cangated2.ino
+++ cangated2/cangated2.ino
@@ -117,8 +117,8 @@ CBUSSwitch pb_switch;               // switch object
 //CBUS definitions
 /********************************************************************************************/
   #define GREEN_LED 4               //MERG Green (SLIM) LED port
-  #define YELLOW_LED 5              //MERG Yellow (FLIM) LED port
-  #define PUSH_BUTTON 6             //std merg FLIM / SLIM push button
+  #define YELLOW_LED 7              //MERG Yellow (FLIM) LED port
+  #define PUSH_BUTTON 8             //std merg FLIM / SLIM push button
   //#define HAS_BUTTON 1            // Define this if there is a button
   //#define PUSH_BUTTON1 7          //debug push button
   #define NODE_VARS 1               //sets up number of NVs for module to store variables
